9f9f5240cead1903de13f9781bb62c7b032d2ab8
Mintesnot Erstu <teklemintesnot2@gmail.com>
2025-08-01 14:01:37 +0300

    fix: correct table name casing in Statements.sql and add .env.examples for database configuration

--- SQL/Statements.sql
+++ SQL/Statements.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS Statements (
+CREATE TABLE IF NOT EXISTS statements (
       dp_id VARCHAR(6) not null PRIMARY KEY,
       amount DECIMAL(10,2) NOT NULL,,
       deposit_date datetime DEFAULT NULL
