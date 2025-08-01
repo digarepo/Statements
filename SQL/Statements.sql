cdee6771c6d92895852d36ba5e93aa9c46c99713
Mintesnot Erstu <teklemintesnot2@gmail.com>
2025-08-01 13:56:37 +0300

    fix: correct data type for amount column in Statements table

--- SQL/Statements.sql
+++ SQL/Statements.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Statements (
       dp_id VARCHAR(6) not null PRIMARY KEY,
-      amount INT NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,,
       deposit_date datetime DEFAULT NULL
     )
